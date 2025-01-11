9b5557490e561fe8c75ef501967d51f28e8d91b3
Shubham Kashyap <shubhamkashyap7200@gmail.com>
2025-01-11 16:16:01 +0530

    Day 12 Done

--- Day 12.playground/Contents.swift
+++ Day 12.playground/Contents.swift
@@ -139,3 +139,166 @@ user2.username
 user1.username
 
 // If you want to create a unique copy of a class instance – sometimes called a deep copy – you need to handle creating a new instance and copy across all your data safely
+
+// The technical term for this distinction is “value types vs reference types.” Structs are value types, which means they hold simple values such as the number 5 or the string “hello”. It doesn’t matter how many properties or methods your struct has, it’s still considered one simple value like a number. On the other hand, classes are reference types, which means they refer to a value somewhere else.
+
+// using a class rather than a struct sends a strong message that you want the data to be shared somehow, rather than having lots of distinct copies
+
+class Calculator {
+    var currentTotal = 0
+}
+var baseModel = Calculator()
+var casio = baseModel
+var texas = baseModel
+casio.currentTotal = 556
+texas.currentTotal = 384
+print(casio.currentTotal)
+print(texas.currentTotal)
+
+// Class Deinialiser
+// Your deinitializer will automatically be called when the final copy of a class instance is destroyed. That might mean it was created inside a function that is now finishing, for example.
+
+// Structs don’t have deinitializers, because you can’t copy them.
+
+class NUser {
+    let id: Int
+    
+    init(id: Int) {
+        self.id = id
+        print("User \(id): I'm alive!")
+    }
+    
+    deinit {
+        print("User \(id): I'm dead!")
+    }
+}
+
+for i in 1...3 {
+    let user = NUser(id: i)
+    print("User \(user.id): I'm in control")
+}
+
+var users = [NUser]()
+
+for i in 1...3 {
+    let user = NUser(id: i)
+    print("User \(user.id): I'm in control!")
+    users.append(user)
+}
+
+print("Loop is finished!")
+users.removeAll()
+print("Array is clear!")
+
+// Behind the scenes Swift performs something called automatic reference counting, or ARC. ARC tracks how many copies of each class instance exists: every time you take a copy of a class instance Swift adds 1 to its reference count, and every time a copy is destroyed Swift subtracts 1 from its reference count. When the count reaches 0 it means no one refers to the class any more, and Swift will call its deinitializer and destroy the object.
+
+//struct Animals: ~ Copyable {
+//    var numberOfLegs: Int
+//    
+//    init(numberOfLegs: Int) {
+//        self.numberOfLegs = numberOfLegs
+//    }
+//    
+//    deinit {
+//        print("I'm dead")
+//    }
+//}
+
+class OUser {
+    var name = "Paul"
+}
+
+let user = OUser()
+user.name = "Taylor" // Putting new name to old user with same posting
+print(user.name)
+
+//user = OUser() // No possible because we pointing to a new user by reference
+//print(user.name)
+
+// How Reference Works !!
+// Constant instance, constant property – a signpost that always points to the same user, who always has the same name.
+// Constant instance, variable property – a signpost that always points to the same user, but their name can change.
+// Variable instance, constant property – a signpost that can point to different users, but their names never change.
+// Variable instance, variable property – a signpost that can point to different users, and those users can also change their names.
+
+
+var number = 5 // Everytime values changes in a struct. Whole struct is created
+number = 6 // That SwiftUI works with struct for UI and classes for data
+
+//class School {
+//    let students = 200
+//    func expel(student: String, for reason: String) {
+//        print("\(student) has been expelled for \(reason).")
+//        students -= 1 // You changing count of student
+//    }
+//}
+
+struct Kindergarten {
+    var numberOfScreamingKids = 30
+    mutating func handOutIceCream() {
+        numberOfScreamingKids = 0
+    }
+}
+let kindergarten = Kindergarten()
+//kindergarten.handOutIceCream()
+
+// Checkpoint 7
+class Animal {
+    var numberOfLegs: Int
+    
+    init(numberOfLegs: Int) {
+        self.numberOfLegs = numberOfLegs
+    }
+}
+
+class Dog: Animal {
+    init() {
+        super.init(numberOfLegs: 4)
+    }
+    
+    func speak() {
+        print("Generic Dog Voice - Bow, Bow, Bow")
+    }
+}
+
+class Persian: Dog {
+    override func speak() {
+        print("Persian Voice - Ba, Ba, Ba")
+    }
+}
+
+class Lion: Dog {
+    override func speak() {
+        print("Lion Voice - Raw, Raw, Raw")
+    }
+}
+
+class Cat: Animal {
+    var isTame: Bool
+    
+    init(isTame: Bool) {
+        self.isTame = isTame
+        super.init(numberOfLegs: 4)
+    }
+    
+    func speak() {
+        print("Generic Cat Voice - Meow, Meow, Meow")
+    }
+}
+
+class Corgi: Cat {
+    
+    override func speak() {
+        print("Corgi Voice - Mi, Mi, Mi")
+    }
+}
+
+class Poodle: Cat {
+    override func speak() {
+        print("Poodle Voice - Mow, Mow, Mow")
+    }
+}
+
+let animal = Animal(numberOfLegs: 4)
+let corgiCat = Corgi(isTame: true)
+corgiCat.speak()
